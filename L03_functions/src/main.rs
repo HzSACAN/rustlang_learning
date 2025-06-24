fc7c6fbfc595d56f39992ff33ddb933a156838d8
Hazret Saçan <asklanhazret51@gmail.com>
2025-06-24 21:44:06 +0300

    functions lesson added

--- L03_functions/src/main.rs
+++ L03_functions/src/main.rs
@@ -1,3 +1,38 @@
 fn main() {
-    println!("Hello, world!");
+    greetings("HzSACAN");
+
+    let value_1 = 10.0;
+    let value_2 = 20.47;
+    let total = sum(value_1, value_2);
+    println!("The total is {}", total);
+
+    let numbers = [3, 6, 1, 7, 0, 9, 4, 8, 4, 7, 3];
+    println!("{:?}", numbers);
+
+    let even_numbers = get_evens(&numbers);
+    println!("{:?}", even_numbers);
+
+    let odd_numbers = get_odds(&numbers);
+    println!("{:?}", odd_numbers);
+}
+
+fn greetings(name: &str) {
+    println!("Hello, {}!", name);
+}
+fn sum(x: f32, y: f32) -> f32 {
+    x + y
+}
+
+fn get_odds(numbers: &[i32]) -> Vec<i32> {
+    let mut result = Vec::new();
+    for n in numbers {
+        if n % 3 == 0 {
+            result.push(*n);
+        }
+    }
+    return result;
+}
+
+fn get_evens(numbers: &[i32]) -> Vec<i32> {
+    numbers.iter().filter(|&&n| n % 2 == 0).cloned().collect()
 }
